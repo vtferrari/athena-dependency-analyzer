8a16100310db858b473e7f8580e2ec7623654dde
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2017-11-08 10:18:24 -0200

    Added react-bootstrap.

--- app/src/main/frontend/app/App.js
+++ app/src/main/frontend/app/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../style/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Athena Dependency Analyzer</h1>
-        </header>
-      </div>
+        <div>
+          <link rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+          <header>
+            <h1>Athena Dependency Analyzer</h1>
+          </header>
+          <p class="text-right">By Netshoes</p>
+        </div>
     );
   }
 }
