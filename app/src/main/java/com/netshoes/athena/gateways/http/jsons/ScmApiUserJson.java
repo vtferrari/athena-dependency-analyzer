45e4b637fedde771ae499dda060b767a5d315893
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2017-11-09 18:20:32 -0200

    Swagger doc for ScmApiUserJson

--- app/src/main/java/com/netshoes/athena/gateways/http/jsons/ScmApiUserJson.java
+++ app/src/main/java/com/netshoes/athena/gateways/http/jsons/ScmApiUserJson.java
@@ -4,16 +4,26 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.netshoes.athena.domains.ScmApiUser;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 @JsonInclude(Include.NON_NULL)
 @ApiModel(value = "ScmApiUser")
 public class ScmApiUserJson {
+  @ApiModelProperty(value = "Login of user in SCM", required = true)
   private final String login;
+
+  @ApiModelProperty(value = "Name of user in SCM")
   private final String name;
+
+  @ApiModelProperty(value = "Request limit of this user to SCM API", required = true)
   private final Integer requestLimit;
+
+  @ApiModelProperty(value = "Remaining requests of this user to SCM API", required = true)
   private final Integer remainingRequests;
+
+  @ApiModelProperty(value = "Error message in case of authentication problem")
   private final String authenticationError;
 
   public ScmApiUserJson(ScmApiUser domain) {
