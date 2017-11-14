dae3dc46d60cc9fab2a0326748432c0b43d04862
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2017-11-14 11:30:31 -0200

    Pagination added to List of Projects in UI.

--- app/src/main/frontend/app/components/ProjectsList.js
+++ app/src/main/frontend/app/components/ProjectsList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Table from 'react-bootstrap/lib/Table';
+import {Pagination, Table} from 'react-bootstrap';
 import {connect} from 'react-redux'
 import {listProjects} from './redux/actions';
 import {bindActionCreators} from 'redux'
@@ -7,7 +7,11 @@ import {bindActionCreators} from 'redux'
 export class ProjectsList extends Component {
 
   componentWillMount() {
-    this.props.listProjects();
+    this.props.listProjects(this.props.pageNumber, this.props.pageSize);
+  }
+
+  handlePagination(pageNumber) {
+    this.props.listProjects(pageNumber - 1, this.props.pageSize);
   }
 
   render() {
@@ -15,25 +19,36 @@ export class ProjectsList extends Component {
     for (var i in this.props.projects) {
       var item = this.props.projects[i];
       rows.push(<tr key={item.projectId}>
-        <td>{item.scmRepository.name}</td>
+        <td>{item.name}</td>
         <td>{item.branch}</td>
         <td>{item.scmRepository.url}</td>
       </tr>)
     }
 
     return (
-        <Table striped={true} hover={true}>
-          <thead>
-          <tr>
-            <th>Name</th>
-            <th>Branch</th>
-            <th>SCM URL</th>
-          </tr>
-          </thead>
-          <tbody>
-          {rows}
-          </tbody>
-        </Table>
+        <div>
+          <Table striped={true} hover={true}>
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>Branch</th>
+              <th>SCM URL</th>
+            </tr>
+            </thead>
+            <tbody>
+            {rows}
+            </tbody>
+          </Table>
+          <div className={"text-center"}>
+            <Pagination activePage={this.props.pageNumber + 1}
+                        items={this.props.totalPages} maxButtons={5}
+                        first={true}
+                        prev={true}
+                        next={true}
+                        last={true}
+                        onSelect={this.handlePagination.bind(this)}/>
+          </div>
+        </div>
     );
   }
 }
@@ -41,6 +56,9 @@ export class ProjectsList extends Component {
 const mapStateToProps = (state) => {
   return {
     projects: state.projectListReducer.projects,
+    pageSize: state.projectListReducer.pageSize,
+    pageNumber: state.projectListReducer.pageNumber,
+    totalPages: state.projectListReducer.totalPages,
     loading: state.projectListReducer.loading
   }
 };
--- app/src/main/frontend/app/components/redux/actions.js
+++ app/src/main/frontend/app/components/redux/actions.js
@@ -1,24 +1,28 @@
 import {RECEIVE_PROJECTS, REQUEST_PROJECTS} from './actionTypes'
 import axios from 'axios'
 
-function requestProjects() {
+function requestProjects(pageNumber, pageSize) {
   return {
-    type: REQUEST_PROJECTS
+    type: REQUEST_PROJECTS,
+    pageNumber: pageNumber,
+    pageSize: pageSize
   }
 }
 
-function receiveProjects(projects) {
+function receiveProjects(data) {
   return {
     type: RECEIVE_PROJECTS,
-    projects: projects,
+    projects: data.items,
+    totalPages: data.totalPages,
+    totalItems: data.totalItems,
     receivedAt: Date.now()
   }
 }
 
-export function listProjects() {
+export function listProjects(pageNumber, pageSize) {
   return function (dispatch) {
-    dispatch(requestProjects());
-    axios.get('http://localhost:8080/api/v1/projects').then(
+    dispatch(requestProjects(pageNumber,pageSize));
+    axios.get('http://localhost:8080/api/v1/projects?pageNumber=' + pageNumber + '&pageSize=' + pageSize).then(
         response => response,
         error => console.log('An error occurred.', error)
     ).then(response => dispatch(receiveProjects(response.data)));
