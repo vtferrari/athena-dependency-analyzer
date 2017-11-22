7dc38b710d74ec5654d8fe80685f7609572f1cc2
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2017-11-22 13:52:00 -0200

    Layout tag from Ant Design

--- app/src/main/frontend/app/containers/App.js
+++ app/src/main/frontend/app/containers/App.js
@@ -2,41 +2,41 @@ import React, {Component} from 'react';
 import ProjectsList from '../components/ProjectsList/ProjectsList';
 import DescriptorsList from '../components/DescriptorsList/DescriptorsList';
 import ArtifactsList from "../components/ArtifactsList/ArtifactsList";
-import {Col, Row} from 'antd';
+import {Col, Layout, Row} from 'antd';
 import '../../style/App.css';
+
+const {Header, Footer, Content} = Layout;
 
 export default class App extends Component {
   render() {
     return (
         <div>
-          <Row>
-            <Col span={24}>
-              <header>
-                <h1>Athena Dependency Analyzer</h1>
-              </header>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={12}>
-              <ProjectsList title={"Projects"}/>
-            </Col>
-            <Col span={12}>
+          <Layout>
+            <Header className={"header"}><h1>Athena Dependency Analyzer</h1>
+            </Header>
+            <Content>
               <Row>
-                <Col span={24}>
-                  <DescriptorsList title={"Descriptors"}/>
+                <Col span={12}>
+                  <ProjectsList title={"Projects"}/>
+                </Col>
+                <Col span={12}>
+                  <Row>
+                    <Col span={24}>
+                      <DescriptorsList title={"Descriptors"}/>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={24}>
+                      <ArtifactsList title={"Artifacts"}/>
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
-              <Row>
-                <Col span={24}>
-                  <ArtifactsList title={"Artifacts"}/>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-
-          <footer>
-            <p className="text-right">By Netshoes</p>
-          </footer>
+            </Content>
+            <Footer className={"footer"}>
+              By Netshoes
+            </Footer>
+          </Layout>
         </div>
     );
   }
