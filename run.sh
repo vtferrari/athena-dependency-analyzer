6d60dfd19ef34a66a50c1294d36ba9568a62d1dc
Caio Oliveira <caio@caiooliveira.eti.br>
2018-04-20 09:29:40 -0300

    Fix in rabbitmq config (#6)
    * Fix in rabbitmq config in docker-compose

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 # Grupo Netshoes
 
 # startup parameters
-export MONGO=$1			# URI to MongoDB. ex: mongodb://localhost:27017/athena
-export RABBITMQ=$2		# RabbitMQ hostname or IP address
-export GITHUB_HOST=$3		# GitHub API HOST
+export MONGO_URI=$1			    # URI to MongoDB. ex: mongodb://localhost:27017/athena
+export RABBITMQ_ADDRESSES=$2	# Addresses to RabbitMQ ex: localhost:5672
+export GITHUB_HOST=$3			# GitHub API HOST
 export GITHUB_ORGANIZATION=$4	# GitHub Organization
-export GITHUB_TOKEN=$5	# GitHub Token
-export ADMIN_USERNAME=$6	# Username for admin
-export ADMIN_PASSWORD=$7	# Password for admin
+export GITHUB_TOKEN=$5	        # GitHub Token
+export ADMIN_USERNAME=$6	    # Username for admin
+export ADMIN_PASSWORD=$7	    # Password for admin
 
 exec $(type -p java) \
   -jar /opt/athena-dependency-analyzer.jar \
-  --spring.data.mongodb.uri=${MONGO} \
-  --spring.rabbitmq.host=${RABBITMQ} \
+  --spring.data.mongodb.uri=${MONGO_URI} \
+  --spring.rabbitmq.addresses=${RABBITMQ_ADDRESSES} \
   --application.github.host=${GITHUB_HOST} \
   --application.github.organization=${GITHUB_ORGANIZATION} \
   --application.github.token=${GITHUB_TOKEN} \
