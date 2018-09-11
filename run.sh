a17c32c63b78b06249bb0fa5cc18746926e449d2
corea <bruno.corea@netshoes.com>
2018-09-11 16:44:49 -0300

    Infra parameters

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 # Grupo Netshoes
 
 # startup parameters
-export MONGO_URI=$1			    # URI to MongoDB. ex: mongodb://localhost:27017/athena
-export RABBITMQ_ADDRESSES=$2	# Addresses to RabbitMQ ex: localhost:5672
+export MONGO_URI=$0			    # URI to MongoDB. ex: mongodb://localhost:27017/athena
+export RABBITMQ_ADDRESSES=$1	# Addresses to RabbitMQ ex: localhost:5672
+export RABBITMQ_HOST=$2     # Rabbitmq vhost
 export RABBITMQ_USER=$3     #User for rabbitmq
 export RABBITMQ_PASS=$4     #Pass for rabbitmq
 export GITHUB_TOKEN=$5	        # GitHub Token
@@ -18,6 +19,7 @@ exec $(type -p java) \
   -jar /opt/athena-dependency-analyzer.jar \
   --spring.data.mongodb.uri=${MONGO_URI} \
   --spring.rabbitmq.addresses=${RABBITMQ_ADDRESSES} \
+  --spring.rabbitmq.host=${RABBITMQ_HOST} \
   --spring.rabbitmq.user=${RABBITMQ_USER} \
   --spring.rabbitmq.pass=${RABBITMQ_PASS} \
   --application.github.token=${GITHUB_TOKEN} \
